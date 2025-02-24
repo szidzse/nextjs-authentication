331f8893ac0c54a827f48e5f0e5cf48b047c4323
Sós Gellért <gellertsos@gmail.com>
2025-02-24 22:57:58 +0100

    create login-button

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const onClick = () => {
+    console.log("Login button clicked.");
+  };
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
