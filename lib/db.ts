9bd61f0a27535e48edea593a2b2665955bb9bab2
Sós Gellért <gellertsos@gmail.com>
2025-02-25 16:52:42 +0100

    create global prisma client

---
+++ lib/db.ts
@@ -0,0 +1,19 @@
+/*import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}*/
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
