9784bb57dc7c8076c3e8699223dec072c614f850
Sós Gellért <gellertsos@gmail.com>
2025-02-25 20:53:43 +0100

    implement authorize function in auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,38 @@
-import GitHub from "next-auth/providers/github";
+import * as bcrypt from "bcryptjs";
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 /**
  * Configuration file for NextAuth authentication.
  * Defines available authentication providers.
  */
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          // OAuth doesn't require password for authentication
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) {
+            return user;
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
