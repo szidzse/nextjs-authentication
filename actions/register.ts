cfa5c36dd12d36d549d3d4b3d5c0740777ff3f62
Sós Gellért <gellertsos@gmail.com>
2025-02-25 17:36:18 +0100

    implement register action

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import { z } from "zod";
 import { RegisterSchema } from "@/schemas";
+import * as bcrypt from "bcryptjs";
+import { prisma } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +12,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use." };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO : Send verification token email
+
+  return { success: "User created." };
 };
