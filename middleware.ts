944bc1fac256e574ad6983b385b2b60693f9a2e2
Sós Gellért <gellertsos@gmail.com>
2025-02-25 20:27:54 +0100

    create routes.ts for public, privaate paths, and implement middleware auth logic

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
+import {
+  publicRoutes,
+  authRoutes,
+  apiAuthPrefix,
+  DEFAULT_LOGIN_REDIRECT,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
@@ -8,9 +14,30 @@ const { auth } = NextAuth(authConfig);
  * to specific routes defined in the matcher array.
  */
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE: ", req.nextUrl.pathname);
-  console.log("IS LOGGED IN: ", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
 });
 
 /**
