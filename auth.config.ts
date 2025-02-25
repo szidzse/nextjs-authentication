cf86d386929fcfcd3c984bd59de8a8d9b482043d
Sós Gellért <gellertsos@gmail.com>
2025-02-25 19:01:52 +0100

    write short documentation for auth fiels

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,8 @@
 import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
 
+/**
+ * Configuration file for NextAuth authentication.
+ * Defines available authentication providers.
+ */
 export default { providers: [GitHub] } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -3,6 +3,12 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import authConfig from "@/auth.config";
 
+/**
+ * Initializes NextAuth with PrismaAdapter (PrismaAdapter is used to allow Auth.js to manage user data via Prisma ORM.)
+ * for database interactions.
+ *
+ * Exports authentication functions for use in the application.
+ */
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
