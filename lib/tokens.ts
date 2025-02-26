d6294ea8d192f71d9e4b85733ac9c0b539e04245
Sós Gellért <gellertsos@gmail.com>
2025-02-26 18:35:47 +0100

    implement generatePasswordResetToken fn

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,34 @@
 import { prisma } from "@/lib/prisma";
 import { v4 as uuidv4 } from "uuid";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+
+  // Expiration Time = Current Time + 1 Hour in Milliseconds
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await prisma.passwordResetToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const passwordResetToken = await prisma.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
