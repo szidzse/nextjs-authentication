bb270fb263842903aba68a863dcd76e72170e918
Sós Gellért <gellertsos@gmail.com>
2025-02-26 21:16:15 +0100

    implement generateTwoFactorToken fn

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,35 @@
 import { prisma } from "@/lib/prisma";
 import { v4 as uuidv4 } from "uuid";
+import crypto from "crypto";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+
+export const generateTwoFactorToken = async (email: string) => {
+  // Generate a 6-digit number then convert to string
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+
+  // Expiration Time = Current Time + 1 Hour in Milliseconds
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await prisma.twoFactorToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const twoFactorToken = await prisma.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
+};
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
