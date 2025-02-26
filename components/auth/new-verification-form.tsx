178d86cdb265cdd57cdaaef8ce5b71465068a590
Sós Gellért <gellertsos@gmail.com>
2025-02-26 16:57:34 +0100

    start implement new-verification-form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,5 +1,32 @@
 "use client";
 
+import { useCallback, useEffect } from "react";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+
 export const NewVerificationForm = () => {
-  return <div>New verification form</div>;
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    console.log(token);
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to Login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        <BeatLoader />
+      </div>
+    </CardWrapper>
+  );
 };
