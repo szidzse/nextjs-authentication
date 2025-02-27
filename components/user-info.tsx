b86f4812352b1c7bf52c8732be05656f086019c8
Sós Gellért <gellertsos@gmail.com>
2025-02-27 12:35:40 +0100

    finish UserInfo component, add variant to shadcn badge

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -1,5 +1,6 @@
 import { ExtendedUser } from "@/next-auth";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 interface UserInfoProps {
   user?: ExtendedUser;
@@ -43,9 +44,9 @@ export const UserInfo = ({ user, label }: UserInfoProps) => {
 
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-medium">Two Factor Authentication</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
-            OFF
-          </p>
+          <Badge variant={user?.isTwoFactorEnabled ? "success" : "destructive"}>
+            {user?.isTwoFactorEnabled ? "ON" : "OFF"}
+          </Badge>
         </div>
       </CardContent>
     </Card>
