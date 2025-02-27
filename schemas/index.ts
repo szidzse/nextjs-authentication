e487a089cbfa31751e32c6488ab8b946041e153b
Sós Gellért <gellertsos@gmail.com>
2025-02-27 16:44:42 +0100

    create SettingsSchema and settings.ts action

--- schemas/index.ts
+++ schemas/index.ts
@@ -27,3 +27,7 @@ export const NewPasswordSchema = z.object({
     message: "Minimum 6 characters required.",
   }),
 });
+
+export const SettingsSchema = z.object({
+  name: z.optional(z.string()),
+});
