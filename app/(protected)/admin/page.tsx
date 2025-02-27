4492bd26634c4233504b5f07c1d1f1875033640e
Sós Gellért <gellertsos@gmail.com>
2025-02-27 13:37:14 +0100

    create role-gate component

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { useCurrentRole } from "@/hooks/use-current-role";
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-success";
+import { UserRole } from "@prisma/client";
 
 const AdminPage = () => {
-  const role = useCurrentRole();
-
   return (
     <Card className="w-[600px]">
       <CardHeader>
         <p className="text-2xl font-semibold text-center">🔑 Admin </p>
       </CardHeader>
-      <CardContent className="space-y-4"></CardContent>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are allowed to view this content." />
+        </RoleGate>
+      </CardContent>
     </Card>
   );
 };
