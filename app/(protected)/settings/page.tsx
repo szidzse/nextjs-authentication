6cba652db8f6b5d96b5e0111c929d66d5b66bea3
Sós Gellért <gellertsos@gmail.com>
2025-02-27 17:25:09 +0100

    implement settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,58 @@
 "use client";
 
-import { useTransition } from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { settings } from "@/actions/settings";
 import { useSession } from "next-auth/react";
+import { SettingsSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { toast } from "sonner";
 
-const SettingPage = () => {
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
   const { update } = useSession();
   const [isPending, startTransition] = useTransition();
 
-  const onClick = () => {
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: user?.name || undefined,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
-      settings({
-        name: "something different",
-      }).then(() => {
-        update();
-      });
+      settings(values)
+        .then((data) => {
+          if (data.error) {
+            setError(data.error);
+            toast.error("Failed to update user infos.");
+          }
+
+          if (data.success) {
+            update();
+            setSuccess(data.success);
+            toast.success("User infos updated successfully.");
+          }
+        })
+        .catch(() => {
+          setError("Something went wrong.");
+        });
     });
   };
 
@@ -26,12 +62,33 @@ const SettingPage = () => {
         <p className="text-2xl font-semibold text-center">⚙️ Settings</p>
       </CardHeader>
       <CardContent>
-        <Button onClick={onClick} disabled={isPending}>
-          Update Name
-        </Button>
+        <Form {...form}>
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="John Doe"
+                        {...field}
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit">Save</Button>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
 };
 
-export default SettingPage;
+export default SettingsPage;
