12b28f25ebc25a32bb10400443d6ca288b7935b9
Sós Gellért <gellertsos@gmail.com>
2025-02-27 21:03:11 +0100

    improve setting page

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,10 +1,13 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 import { SettingsSchema } from "@/schemas";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -25,6 +28,39 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     values.password = undefined;
     values.newPassword = undefined;
     values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: "Email already in use." };
+    }
+
+    const verificationToken = await generateVerificationToken(values.email);
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token,
+    );
+
+    return { success: "Verification email sent." };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordsMatch = await bcrypt.compare(
+      values.password,
+      dbUser.password,
+    );
+
+    if (!passwordsMatch) {
+      return { error: "Incorrect password." };
+    }
+
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+
+    values.password = hashedPassword;
+    values.newPassword = undefined;
   }
 
   await prisma.user.update({
