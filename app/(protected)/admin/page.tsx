26f723c972e637fa9d991ffd4b368229a8f5376a
Sós Gellért <gellertsos@gmail.com>
2025-02-27 13:15:57 +0100

    create admin page

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useCurrentRole } from "@/hooks/use-current-role";
+
+const AdminPage = () => {
+  const role = useCurrentRole();
+
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">🔑 Admin </p>
+      </CardHeader>
+      <CardContent className="space-y-4"></CardContent>
+    </Card>
+  );
+};
+
+export default AdminPage;
